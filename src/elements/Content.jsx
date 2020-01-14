79fcd5562407b0ff72b5bac965a894b6468edbb9
Christoph Flick <christophflick@gmx.de>
2020-01-14 10:23:18 +0100

    Inital commit with hugo

--- src/elements/Content.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import tw from 'tailwind.macro'
-
-const Content = styled.div`
-  min-height: ${props => props.full ? '100%;' : '100vh;'}
-  ${tw`p-6 md:p-12 lg:p-24 justify-center items-center flex z-50`};
-`
-
-export default Content
